b62189e4956f5daa5471d43c49bfcd5f391b71ea
Wyatt Joyner <wcj3te@virginia.edu>
2019-03-11 19:38:10 -0400

    eel generator

---
+++ Steel Eel/Assets/Scripts/EelGenerate.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EelGenerate : MonoBehaviour
+{
+
+    public float radius = -1;
+    private float offset;
+
+    public int segments = 5;
+
+    private GameObject last_slice;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject child = transform.GetChild(0).gameObject;
+
+        if (radius == -1)
+        {
+            radius = child.GetComponent<CircleCollider2D>().radius;
+        }
+
+        radius *= 2;
+        offset = radius;
+        last_slice = child;
+
+        while (segments > 0)
+        {
+            GenerateSlice(child);
+            segments--;
+        }
+    }
+
+    void GenerateSlice(GameObject child)
+    {
+        GameObject slice = Instantiate(child, transform);
+        slice.transform.Translate(new Vector2(0, -offset));
+
+        last_slice.GetComponent<HingeJoint2D>().connectedBody = slice.GetComponent<Rigidbody2D>();
+        slice.GetComponent<HingeJoint2D>().connectedBody = last_slice.GetComponent<Rigidbody2D>();
+
+        last_slice = slice;
+        offset += radius;
+    }
+}
