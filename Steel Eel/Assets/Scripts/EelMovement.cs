83004cbefa8363ee113747a5b2c8a5aeebe5518f
Ryan Kann <ryanakann@gmail.com>
2019-03-11 19:37:36 -0400

    Movement system

--- Steel Eel/Assets/Scripts/EelMovement.cs
+++ Steel Eel/Assets/Scripts/EelMovement.cs
@@ -2,17 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EelMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+[RequireComponent(typeof(Rigidbody2D))]
+public class EelMovement : MonoBehaviour {
+	public bool showGizmos = true;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[Header("Mouse Properties")]
+	public bool lookAtMouse = true;
+	public float sensitivity = 1f;
+	public float maxMouseDistance = 3f;
+
+	[Header("Speed Properties")]
+	public Vector2 speedRange;
+	public float dashSpeed = 5f;
+	public float maxDashCooldown = 2f;
+	[SerializeField] private float currentDashCooldown;
+
+	private Ray mouseScreenRay;
+	private float mouseRayDistance;
+	private Vector2 mouseWorldPosition;
+	private Camera mainCamera;
+	private Plane playArea;
+
+	private Rigidbody2D rb;
+	private Vector2 targetDirection;
+	private float targetSpeed;
+	private Vector2 targetVelocity;
+	private Vector2 refVelocity;
+
+	private void Start () {
+		mainCamera = Camera.main;
+		playArea = new Plane(Vector3.forward, 0f); //Assumes player always at z = 0
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate () {
+		GetMousePosition();
+
+		rb.velocity = Vector2.SmoothDamp(rb.velocity, targetVelocity, ref refVelocity, 1 / sensitivity);
+
+		if (currentDashCooldown <= 0) {
+			if (Input.GetMouseButtonDown(0)) {
+				Dash();
+			}
+			currentDashCooldown = 0;
+		} else {
+			currentDashCooldown -= Time.fixedDeltaTime;
+		}
+	}
+
+	private void Dash () {
+		rb.velocity = (mouseWorldPosition - (Vector2)transform.position).normalized * dashSpeed;
+		currentDashCooldown = maxDashCooldown;
+	}
+
+	private void GetMousePosition () {
+		mouseScreenRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+		if (playArea.Raycast(mouseScreenRay, out mouseRayDistance)) {
+			mouseWorldPosition = mouseScreenRay.GetPoint(mouseRayDistance);
+			targetDirection = (mouseWorldPosition - (Vector2)transform.position) / maxMouseDistance;
+
+			if (targetDirection.sqrMagnitude > 1f) {
+				targetDirection.Normalize();
+			}
+
+			targetSpeed = Mathf.Lerp(speedRange.x, speedRange.y, targetDirection.magnitude);
+			targetVelocity = targetDirection.normalized * targetSpeed;
+		}
+	}
+
+	private void OnDrawGizmos () {
+		if (!showGizmos) return;
+
+		Gizmos.color = Color.white;
+		Gizmos.DrawLine(transform.position, transform.position + (Vector3)targetVelocity);
+	}
 }
