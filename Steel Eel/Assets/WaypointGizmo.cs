9f46134d03120d5b93a08252e130fef8b26e6980
Ryan Kann <ryanakann@gmail.com>
2019-03-12 12:15:07 -0400

    Level test complete with AI

---
+++ Steel Eel/Assets/WaypointGizmo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointGizmo : MonoBehaviour {
+	public bool gizmosEnabled = false;
+	public Color color;
+	public float pointRadius = 0.25f;
+
+
+	private void OnDrawGizmos () {
+		if (!gizmosEnabled) return;
+
+		Gizmos.color = color;
+		foreach (Transform child in transform) {
+			Gizmos.DrawSphere(child.position, pointRadius);
+		}
+	}
+}
