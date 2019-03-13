232057f453a5f81d38a1ee79016bff4739949f25
Wyatt Joyner <wcj3te@virginia.edu>
2019-03-12 20:48:36 -0400

    doors done

---
+++ Steel Eel/Assets/Scripts/DoorOpener.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour, Interactable
+{
+    private int playerTouches;
+
+    public GameObject circle, door;
+
+    bool interacted;
+
+    private void Start()
+    {
+        circle = transform.GetChild(0).gameObject;
+        door = transform.parent.GetChild(1).gameObject;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTouches > 0 && !interacted)
+        {
+            circle.SetActive(true);
+        }
+        else
+        {
+            circle.SetActive(false);
+        }
+    }
+
+    public void Interact()
+    {
+        if (playerTouches > 0 && !interacted)
+        {
+            circle.SetActive(false);
+            interacted = true;
+            door.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+            playerTouches++;
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+            playerTouches--;
+    }
+}
