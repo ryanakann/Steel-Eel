6771d747bfe027269a18600d2a6897363e2bd8c3
Wyatt Joyner <wcj3te@virginia.edu>
2019-03-12 23:42:30 -0400

    doing menu

---
+++ Steel Eel/Assets/3rd Party/EasyMainMenu/Scripts/Main Menu Scripts/LoadSlots.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSlots : MonoBehaviour
+{
+    public List<GameObject> levels = new List<GameObject>(); 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ResetProgress()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        for (int i = 0; i < levels.Count; i++)
+        {
+            levels[i].SetActive(i < (Loader.instance.lastLevel - 1));
+        }
+    }
+}
